93d2d480a56be7497165a6de81ba6d83be5a886d
sonntex <sonntex@gmail.com>
2017-11-17 10:23:44 +0300

    make dynamic

--- src/rtmp2rtsp.c
+++ src/rtmp2rtsp.c
@@ -2,11 +2,55 @@
 
 #include <gst/rtsp-server/rtsp-server.h>
 
-/* this timeout is periodically run to clean up the expired sessions from the
- * pool. This needs to be run explicitly currently but might be done
- * automatically as part of the mainloop. */
+const guint timeout = 10;
+
+static void
+callback_options_request (GstRTSPClient * client, GstRTSPContext * ctx, GstRTSPServer * server, gpointer user_data)
+{
+  GstRTSPMountPoints *mounts;
+  GstRTSPMediaFactory *factory;
+
+  mounts = gst_rtsp_server_get_mount_points (server);
+
+  factory = gst_rtsp_mount_points_match (mounts, ctx->uri->abspath, NULL);
+
+  if (!factory)
+  {
+    gchar *launch;
+
+    factory = gst_rtsp_media_factory_new ();
+
+    launch = g_strdup_printf (
+        "( rtmpsrc location=rtmp://localhost:1935/rtmp2rtsp%s timeout=%u ! flvdemux name=demux "
+        "  demux.video ! queue ! h264parse ! rtph264pay name=pay0 pt=96 "
+        "  demux.audio ! queue !  aacparse ! rtpmp4apay name=pay1 pt=97 )",
+        ctx->uri->abspath, timeout);
+
+    gst_rtsp_media_factory_set_launch (factory, launch);
+    gst_rtsp_media_factory_set_shared (factory, TRUE);
+
+    gst_rtsp_mount_points_add_factory (mounts, ctx->uri->abspath, factory);
+
+    g_print ("launch %s\n", launch);
+
+    g_free (launch);
+  }
+  else
+  {
+    g_object_unref (factory);
+  }
+
+  g_object_unref (mounts);
+}
+
+static void
+callback_client_connected (GstRTSPServer * server, GstRTSPClient * client, gpointer user_data)
+{
+  g_signal_connect_object (client, "options-request", (GCallback) callback_options_request, server, G_CONNECT_AFTER);
+}
+
 static gboolean
-timeout (GstRTSPServer * server)
+callback_timeout (GstRTSPServer *server)
 {
   GstRTSPSessionPool *pool;
 
@@ -22,73 +66,38 @@ main (int argc, char *argv[])
 {
   GMainLoop *loop;
   GstRTSPServer *server;
-  GstRTSPMountPoints *mounts;
-  GstRTSPMediaFactory *factory;
-  gchar *location;
   gchar *host;
   gchar *port;
-  gchar *description;
 
-  if (argc != 4) {
+  if (argc != 3) {
     g_print ("failed to parse arguments\n");
     return -1;
   }
 
-  location = argv[1];
-  host = argv[2];
-  port = argv[3];
-
-  description = g_strdup_printf ("( "
-      "rtmpsrc location=%s timeout=10 ! flvdemux name=demux "
-      "demux.video ! queue ! h264parse ! rtph264pay name=pay0 pt=96 config-interval=1 "
-      "demux.audio ! queue ! aacparse  ! rtpmp4apay name=pay1 pt=97 " ")",
-      location);
+  host = argv[1];
+  port = argv[2];
 
   gst_init (&argc, &argv);
 
   loop = g_main_loop_new (NULL, FALSE);
 
-  /* create a server instance */
   server = gst_rtsp_server_new ();
+
   gst_rtsp_server_set_address (server, host);
   gst_rtsp_server_set_service (server, port);
 
-  /* get the mount points for this server, every server has a default object
-   * that be used to map uri mount points to media factories */
-  mounts = gst_rtsp_server_get_mount_points (server);
-
-  /* make a media factory for a test stream. The default media factory can use
-   * gst-launch syntax to create pipelines.
-   * any launch line works as long as it contains elements named pay%d. Each
-   * element with pay%d names will be a stream */
-  factory = gst_rtsp_media_factory_new ();
-  gst_rtsp_media_factory_set_launch (factory, description);
-
-  /* attach the test factory to the /test url */
-  gst_rtsp_mount_points_add_factory (mounts, "/stream", factory);
-
-  /* don't need the ref to the mapper anymore */
-  g_object_unref (mounts);
-
-  /* attach the server to the default maincontext */
-  if (gst_rtsp_server_attach (server, NULL) == 0)
-    goto failed;
-
-  /* add a timeout for the session cleanup */
-  g_timeout_add_seconds (2, (GSourceFunc) timeout, server);
-
-  /* start serving, this never stops */
-  g_print ("stream ready at rtsp://%s:%s/stream\n", host, port);
-  g_main_loop_run (loop);
-
-  g_free (description);
-
-  return 0;
-
-  /* ERRORS */
-failed:
-  {
-    g_print ("failed to attach the server\n");
+  if (gst_rtsp_server_attach (server, NULL) == 0) {
+    g_print ("failed to attach\n");
     return -1;
   }
+
+  g_signal_connect (server, "client-connected", (GCallback) callback_client_connected, NULL);
+
+  g_timeout_add_seconds (timeout, (GSourceFunc) callback_timeout, server);
+
+  g_print ("run at %s:%s\n", host, port);
+
+  g_main_loop_run (loop);
+
+  return 0;
 }
